564145449bfed3f617e2036e0746e38e37d93d33
Tyler Campbell <tyler.campbell@gmail.com>
2015-11-28 10:38:20 -0700

    metadata icons, click functionality

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,12 +1,20 @@
 var tl = new TimelineLite();
-tl.to("#trees-fg-right", 2.5, { x:860, y:100 }, 1)
-  .to("#trees-fg-left",  2.5, { x:-600, y:0 },  1)
+tl.to("#trees-fg-right", 2.5, { x:860, y:100 }, 0)
+  .to("#trees-fg-left",  2.5, { x:-600, y:0 },  0)
 
-  .from("#trees-bg", 2, { y:80, x:20, scale:.8, opacity: 0 }, 1)
-  .from("#grass",    2, { y:20 }, 1)
+  .from("#trees-bg", 2, { y:80, x:20, scale:.8, opacity: 0 }, 0)
+  .from("#grass",    2, { y:20 }, 0)
 
-  .from("#tent-bg",  2, { y:40, x:-30, scale:.7, opacity: .5 }, 1)
-  .from("#tent-fg",  2, { y:-50, scale:.7 }, 1)
+  .from("#tent-bg",  2, { y:40, x:-30, scale:.7, opacity: .5 }, 0)
+  .from("#tent-fg",  2, { y:-50, scale:.7 }, 0)
 
-  .from("#flag", 1, { y:500, scaleX:0 }, 2.5)
-  .from("#bell", 1, { y:500, scaleX:0 }, 2.5);
+  .from("#flag", 1, { y:500, scaleX:0 }, 1.5)
+  .from("#bell", 1, { y:500, scaleX:0 }, 1.5);
+
+function yoyo() {
+    if (tl.reversed()) {
+        tl.play();
+    } else {
+        tl.reverse();
+    }
+  }
