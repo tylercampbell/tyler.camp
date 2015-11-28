b61b6f1fce84bd07f4a1d1938fa3c7dabb39ad26
Tyler Campbell <tyler.campbell@gmail.com>
2015-11-27 17:42:18 -0700

    fixed grass

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,7 @@ tl.from("svg", 2, { opacity:0 }, 0)
   .to("#trees-fg-left",  2.5, { x:-600, y:0 },  0)
 
   .from("#trees-bg", 2, { y:80, x:-20, scale:.8 }, 0)
-  .from("#grass",    2, { y:-120, scale:1.3 },     0)
+  .from("#grass",    2, { y:-150, scale:1.2 },     0)
 
   .from("#tent-bg",  2, { y:-50, scale:.7 }, 0)
   .from("#tent-fg",  2, { y:-50, scale:.7 }, 0)
