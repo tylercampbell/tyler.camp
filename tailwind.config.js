0a2974ca3095a19c7e37086564040000f15268d7
Tyler Campbell <tylercampbell@users.noreply.github.com>
2021-07-25 15:01:00 -0400

    remove animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,27 +4,4 @@ module.exports = {
     './**/*.html'
   ],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-        keyframes: {
-            wiggle: {
-                '0%, 100%': {
-                    transform: 'rotate(-3deg)'
-                },
-                '50%': {
-                    transform: 'rotate(3deg)'
-                },
-            }
-        },
-        animation: {
-            wiggle: 'wiggle 3s ease-in-out infinite',
-        }
-    },
-  },
-  variants: {
-    extend: {
-      animation: ['hover', 'focus'],
-    },
-  },
-  plugins: [],
 }
