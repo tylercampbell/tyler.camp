bc4babd2d930c4bf6732447451968ffebef3dfc2
Tyler Campbell <tylercampbell@users.noreply.github.com>
2021-07-27 10:59:34 -0400

    code cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,4 @@ module.exports = {
   mode: 'jit',
   purge: ['./**/*.html'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
