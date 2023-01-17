f4ab456b5d72b9bb55749466bee4f27afe357094
tylercampbell <tylercampbell@users.noreply.github.com>
2023-01-16 23:56:43 -0500

    move mobile nav to bottom
    + async image function
    + better bg colors

--- src/_11ty/shortcode-image.js
+++ src/_11ty/shortcode-image.js
@@ -1,30 +1,35 @@
 const Image = require("@11ty/eleventy-img");
-Image.concurrency = 8; // default is 10
 
-// Image
-function pictureShortcode(src, alt, classes = "", style = "", sizes = "100vw", loading = "lazy", decoding = "async") {
-  let url = `./src/_images/${src}`;
-  let options = {
+async function asyncImageShortcode(
+  src,
+  alt,
+  tailwind = "",
+  style = "",
+  sizes = "100vw",
+  loading = "lazy",
+  decoding = "async"
+) {
+  let metadata = await Image(src, {
     widths: [480, 770, 1920],
     formats: ["webp", "jpeg"],
-    urlPath: "/img/optimized/",
-    outputDir: "./_site/img/optimized/",
-  };
-  Image(url, options);
+    urlPath: "/img/",
+    outputDir: "./_site/img/",
+  });
+
   let imageAttributes = {
     alt,
-    class: classes,
-    style: style,
+    class: tailwind,
+    style,
     sizes,
     loading,
     decoding,
   };
-  let metadata = Image.statsSync(url, options);
+
   return Image.generateHTML(metadata, imageAttributes, {
     whitespaceMode: "inline",
   });
 }
 
-module.exports = eleventyConfig => {
-  eleventyConfig.addShortcode("picture", pictureShortcode);
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addAsyncShortcode("pic", asyncImageShortcode);
 };
